bc0b783083c6d1a79b2d2b0b5c3652dc04f7f139
Bahar <baharsadeghzade@gmail.com>
2024-02-01 19:20:20 +0100

    few types

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import React from "react";
+// Primitive types
 
-function App() {
-  return <div>React Types</div>;
-}
+import {
+  ComponentProps,
+  ReactElement,
+  ReactFragment,
+  ReactNode,
+  ReactPortal,
+} from "react";
 
-export default App;
+// ReactElement
+// ReactNode
+// Jsx.Element
+// @depricated ReactChild
+
+type ImportantTypes = [
+  ReactElement,
+  ReactNode,
+  ReactPortal,
+  ReactFragment,
+  JSX.Element
+];
+
+/////////////////////////////////////////
+
+// Type helpers
+
+// PropsWithChildren
+// ComponentProps
+
+export const Div = (props: ComponentProps<"div">) => {
+  return <div {...props} />;
+};
+
+const Parent = () => {
+  return <Div></Div>;
+};
